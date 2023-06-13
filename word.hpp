b201665d399fe59159469e4c8bf0a651553ecd30
Alexander Stratton <72987090+als-217@users.noreply.github.com>
2023-06-12 21:04:50 -0500

    Add files via upload

---
+++ word.hpp
@@ -0,0 +1,41 @@
+#ifndef WORD_HPP
+#define WORD_HPP
+
+#include <utility>
+#include <map>
+#include <string>
+#include <vector>
+#include <sstream>
+
+enum class PoS { kMultiple, kVerb, kNoun, kAdv, kAdj, kPrep };
+
+class Word {
+    public:
+        Word() = default;
+        Word(PoS pos, std::string word, std::string defn, std::string tags, std::string form_of, std::string special_char = "");
+        PoS Pos() const;
+        const std::string& GetWord() const;
+        const std::vector<std::string>& Tags() const;
+        const std::vector<std::string>& Defn() const;
+        std::string AltForm() const;
+        void Print(int defn_idxs = -1) const;
+        bool operator==(const Word& word);
+        bool operator<(const Word& word);
+        bool operator<=(const Word& word);
+        bool operator>=(const Word& word);
+        bool operator>(const Word& word);
+        bool operator!=(const Word& word);
+        std::vector<int> Convert(const std::string& sub_str);
+    private:
+        std::vector<std::string> ReadTags(const std::string& tags);
+        std::vector<std::string> ReadDefn(const std::string& defn);
+        int CompareTo(const Word& word) const;
+        PoS pos_ = PoS::kMultiple;
+        std::string word_str_;
+        std::string form_of_ = "0";
+        bool spcl_char_ = false;
+        std::vector<std::string> defn_;
+        std::vector<std::string> tags_;
+};
+
+#endif
